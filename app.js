e6565744ef41ffc76e5100e5878a38e855a27576
Jepoy Factoran <factoran20@gmail.com>
2022-05-02 08:15:13 +0800

    module for checking if player won

--- app.js
+++ app.js
@@ -15,13 +15,28 @@ maxNum.textContent = max;
 
 guessBtn.addEventListener("click", function () {
   let input = parseInt(guessInput.value);
-
+  // check if input is valid
   if (isNaN(input) || input < min || input > max) {
     msg = `Please input a number between ${min} and ${max}`;
-    showMessage(msg);
+    showMessage(msg, "red", false);
+  } else {
+    //check if player won
+    if (input === winningNum) {
+      showMessage("Congrats! you guess that right mate!", "green", true);
+    } else {
+      tries -= 1;
+      if (tries === 0) {
+        showMessage("Sorry your ran out of guesses noob", "red", true);
+      } else {
+        showMessage(`${input} was wrong, you have ${tries} tries left`, "red");
+      }
+    }
   }
 });
 
-function showMessage(msg) {
+function showMessage(msg, color, disabled) {
   message.textContent = msg;
+  message.style.color = color;
+  guessBtn.disabled = disabled;
+  guessInput.style.borderColor = color;
 }
